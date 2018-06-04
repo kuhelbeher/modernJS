6414a3f84e58b09681a75671da7e481743771cce
Dmytro Bartieniev <kuhelebeher@gmail.com>
2018-06-04 23:11:20 +0300

    Added weatherJS

---
+++ weatherjs/app.js
@@ -0,0 +1,35 @@
+// init classes
+const storage = new LStorage();
+// get stored location data
+const weatherLocation = storage.getLocationData();
+
+const weather = new Weather(weatherLocation.city, weatherLocation.state);
+const ui = new UI();
+
+
+
+// get weather on DOM load
+document.addEventListener('DOMContentLoaded', getWeather);
+
+// change location event
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+	const city = document.getElementById('city').value;
+	const state = document.getElementById('state').value;
+
+	weather.changeLocation(city, state);
+
+	// set location
+	storage.setLocationData(city, state);
+
+	getWeather();
+
+	$('#locModal').modal('hide');
+});
+
+function getWeather() {
+	weather.getWeather()
+	.then(results => {
+		ui.paint(results);
+	})
+	.catch(err => console.log(err));
+}
